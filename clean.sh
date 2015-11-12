63a44e885cb2e85c3eb32a5d9a337b25e839cc4a
AnSwErYWJ <ywj1993@yeah.net>
2015-11-12 10:13:06 +0800

    20151112

--- clean.sh
+++ clean.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-#使环境变量生效
 source /etc/profile
-source /etc/sysconfig/i18n #中文显示
+source /etc/sysconfig/i18n
 
-#目录名
 dir=$(date +%m-%d -d -10days)
+new=$(date +%Y-%m-%d -d -10days)
 
-#不存在这个目录就退出
 if [ ! -d "/media/sdb1_2TB/BB-Data/$dir" ];then
 exit 0
 fi
 
-#进入目录
 cd /media/sdb1_2TB/BB-Data//$dir
 
 if [ -d "var" ];then
 rm -rf var
 fi
 
-for i in *.log
+for i in $new*
 do
-rm -rf *.log
+rm -rf $i
 done
-
 
 for i in *.zip
 do
--- process.sh
+++ process.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#crontab 环境变量生效
 source /etc/profile
-source /etc/sysconfig/i18n #中文显示
+source /etc/sysconfig/i18n
 
-cd /media/sdb1_2TB/BB-Data/data_process #进入目录
+cd /media/sdb1_2TB/BB-Data/data_process
 
-yesterday=$(date +%Y-%m-%d -d -2days) 
-dir_ftp=$(date +%Y%m%d -d -2days) #上传目录名
-dir_name=$(date +%m-%d -d -2days) #本地目录名
-
-list="../$dir_name/url-$yesterday.txt" #下载链接文件
+yesterday=$(date +%Y-%m-%d -d -2days)
+dir_ftp=$(date +%Y%m%d -d -2days)
+dir_name=$(date +%m-%d -d -2days)
+list="../$dir_name/url-$yesterday.txt"
 
 #创建对应目录
 if [ ! -d "../$dir_name" ];then
@@ -20,43 +18,49 @@ fi
 #生成相应下载链接
 awk -F_ '{
             $NF="'$yesterday'.log.tgz"
-            OFS="_" 
+            OFS="_"
             print
         }' raw.txt > $list
 
 
 #批量下载
 start_time=$(date +%s)
+
 wget -c --accept=tgz --http-user=optimizerdl_auth --http-passwd=S0mO4Ei8LjWzkcoeqZtt -i $list -P ../$dir_name
+
 end_time=$(date +%s)
 total_time=`expr $end_time - $start_time`
 total_min=`expr $total_time / 60`
 total_sec=`expr $total_time % 60`
 echo "下载耗时:$total_min 分 $total_sec 秒" > ../$dir_name/time.txt
 
-#sleep 3s
+sleep 3s
 
 #批量解压
 start_time=$(date +%s)
+
 for file in ../$dir_name/*.tgz
 do
     tar -zxvf $file -C ../$dir_name
 done
+
 end_time=$(date +%s)
 total_time=`expr $end_time - $start_time`
 total_min=`expr $total_time / 60`
 total_sec=`expr $total_time % 60`
 echo "解压耗时:$total_min 分 $total_sec 秒" >> ../$dir_name/time.txt
 
-#sleep 3s
+sleep 3s
 
 #批量解析
 cd ../$dir_name
 start_time=$(date +%s)
+
 for log in var/www/neo/logs/*.log
 do
- 	/usr/local/bin/python3 ../data_process/jsontransform.py $log config
+ 	/usr/local/bin/python3 ../data_process/jsontransform.py $log ../data_process/config
 done
+
 end_time=$(date +%s)
 total_time=`expr $end_time - $start_time`
 total_min=`expr $total_time / 60`
@@ -65,17 +69,23 @@ echo "解析耗时:$total_min 分 $total_sec 秒" >> time.txt
 
 sleep 3s
 
-ADDR=111.203.211.2
-PORT=1503
-USER="ftp_up01"
-PWD="dk3_gk3up"
 
 # 批量压缩
 start_time=$(date +%s)
-for dir in *.log
+new=$(date +%Y-%m-%d -d -2days)
+i=1
+
+for dir in *.done
+do
+mv $dir $new-$i
+i=`expr $i + 1`
+done
+
+for dir in $new*
 do
 zip -r $dir.zip $dir
 done
+
 end_time=$(date +%s)
 total_time=`expr $end_time - $start_time`
 total_min=`expr $total_time / 60`
@@ -83,7 +93,14 @@ total_sec=`expr $total_time % 60`
 echo "压缩耗时:$total_min 分 $total_sec 秒" >> time.txt
 
 #上传
+
+ADDR=111.203.211.2
+PORT=1503
+USER="ftp_up01"
+PWD="dk3_gk3up"
+
 #start_time=$(date +%s)
+
 #	ftp  -n <<EOF 
 #	open $ADDR $PORT
 #	user $USER $PWD
@@ -97,6 +114,7 @@ echo "压缩耗时:$total_min 分 $total_sec 秒" >> time.txt
 #	close
 #	bye
 #EOF
+
 #end_time=$(date +%s)
 #total_time=`expr $end_time - $start_time`
 #total_min=`expr $total_time / 60`
